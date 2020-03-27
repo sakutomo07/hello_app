c8db63dbfa9dc71fe947b4c688bf76ff6b3fde8f
sakutomo07 <arai15735320@gmail.com>
2020-03-27 16:38:28 +0900

    Update all file

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class ApplicationController < ActionController::Base
+  def hello
+    render html: '¡Hola, mundo!'
+  end
+
+  def goodbye
+    render html: 'goodbye, world!'
+  end
+
+end
